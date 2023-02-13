8a8404ecc387647a37db5436aad5d573ec427802
Malaviya Krish <120166835+GurukulSK@users.noreply.github.com>
2023-02-14 02:48:17 +0530

    firebace intigration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
+  firebase: {
+    projectId: 'safariapp-70d71',
+    appId: '1:771133664732:web:bd44d743798dc402ddf319',
+    databaseURL: 'https://safariapp-70d71-default-rtdb.firebaseio.com',
+    storageBucket: 'safariapp-70d71.appspot.com',
+    locationId: 'asia-south1',
+    apiKey: 'AIzaSyDSFM30lvAkgN5-AKVyRkBCEMcsOPu-MD8',
+    authDomain: 'safariapp-70d71.firebaseapp.com',
+    messagingSenderId: '771133664732',
+    measurementId: 'G-7VV9LHG5J9',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,28 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  firebase: {
+    projectId: 'safariapp-70d71',
+    appId: '1:771133664732:web:bd44d743798dc402ddf319',
+    databaseURL: 'https://safariapp-70d71-default-rtdb.firebaseio.com',
+    storageBucket: 'safariapp-70d71.appspot.com',
+    locationId: 'asia-south1',
+    apiKey: 'AIzaSyDSFM30lvAkgN5-AKVyRkBCEMcsOPu-MD8',
+    authDomain: 'safariapp-70d71.firebaseapp.com',
+    messagingSenderId: '771133664732',
+    measurementId: 'G-7VV9LHG5J9',
+  },
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDSFM30lvAkgN5-AKVyRkBCEMcsOPu-MD8",
+    authDomain: "safariapp-70d71.firebaseapp.com",
+    databaseURL: "https://safariapp-70d71-default-rtdb.firebaseio.com",
+    projectId: "safariapp-70d71",
+    storageBucket: "safariapp-70d71.appspot.com",
+    messagingSenderId: "771133664732",
+    appId: "1:771133664732:web:bd44d743798dc402ddf319",
+    measurementId: "G-7VV9LHG5J9"
+  };
 };
 
 /*
